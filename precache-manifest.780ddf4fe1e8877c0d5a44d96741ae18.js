bf1dee4f341126d29a50d865d420b0174ce44560
Amila-Pathberiya <32818076+amila1986@users.noreply.github.com>
2019-10-18 18:27:47 +0530

    Updates

---
+++ precache-manifest.780ddf4fe1e8877c0d5a44d96741ae18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cba8db6077352db25a5f9cb3b97d48",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d8438430f22c4c19e57e",
+    "url": "/robofriends/static/css/main.feebe0df.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "d8438430f22c4c19e57e",
+    "url": "/robofriends/static/js/main.0378f853.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
